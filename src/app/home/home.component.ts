7acb8a091a5a39cfbfe5f702cadf185622698010
CassieMaesie <cassie.m.myers@gmail.com>
2024-03-26 19:40:43 -0400

    Added OnInit logic to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
   //styles: ['.red {color:red;}']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor() {} 
 
+  ngOnInit(): void {
+    
+  }
 }
