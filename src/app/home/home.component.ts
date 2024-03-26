f1e9cd876dea1d7cb11309e11b0171a7ecbc9398
CassieMaesie <cassie.m.myers@gmail.com>
2024-03-26 18:15:43 -0400

    Inline component template and styles

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-home',
+  templateUrl: './home.component.html',
+  //template: '<p class="red">Inline home works!</p>',
+  styleUrls: ['./home.component.css']
+  //styles: ['.red {color:red;}']
+})
+export class HomeComponent {
+
+}
